7b88ca94c67785f1e67a52253c8dee049cd1a04d
unknown <iasonas.tr@gmail.com>
2024-07-22 21:30:12 +0300

    updated UDAF functions

--- justfunctions/bigquery/templates/function_example.sql
+++ justfunctions/bigquery/templates/function_example.sql
@@ -1,1 +1,20 @@
-SELECT `{{ project_id }}.{{ dataset_id }}.{{ function_name }}`({% for example in examples %}{% if loop.first %}{{'\"' + example['arguments']|join('","') + '\"'}}{% endif %}{% endfor %})
+{%- for example in examples %}
+{%- if example.with_clause is defined %}
+WITH sample AS (
+{{ example.with_clause }}
+)
+{%- endif %}
+SELECT `{{ project_id }}.{{ dataset_id }}.{{ function_name }}`(
+{%- set result = [] %}
+{%- for i in range(arguments | length) %}
+{%- set argument_type = arguments[i].type %}
+{%- if argument_type.lower() in ['string','date'] %}
+  {%- set _ = result.append('"' + example.arguments[i]|string + '"' ) %}
+{%- else %}
+  {%- set _ =  result.append(example.arguments[i]|string) %}
+{%- endif %}
+{%- endfor %}{{ result | join(', ') }})
+{%- if example.with_clause is defined %}
+FROM sample
+{%- endif %}
+{%- endfor %}
